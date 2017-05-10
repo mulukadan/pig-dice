56c6c3d66b79f7b950054fbbe71a74febe4cf765
mulukadan <kadan.md@gmail.com>
2017-05-10 13:34:39 +0300

    Seting up

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+      $("form").submit(function(event) {
+        var a = parseInt($("input#sidea").val());
+        var b = parseInt($("input#sideb").val());
+        var c = parseInt($("input#sidec").val());
+        var results="";
+
+
+
+      if(c> a+b || b>a+c || a>b+c){
+        results = "Not a Triangle";
+      }else {
+        if(a===b && a===c){
+          results = "Equilateral";
+        }else {
+          if(a===b || a===c || b===c ){
+          results = "Isosceles";
+        }
+        else {
+          if(a!=b && a!=c){
+             results = "Scalene";
+          }
+        }
+      }
+      }
+        setImageSource(results);
+        $(".output").text(results);
+        event.preventDefault();
+      });
+
+      $("#computer").click(function(event) {
+        $("#compimg").attr("src","img/computeractive.png");
+        $("#friendimg").attr("src","img/friends.png");
+
+        // event.preventDefault();
+      });
+      $("#friend").click(function(event) {
+        $("#compimg").attr("src","img/computer.png");
+        $("#friendimg").attr("src","img/friendactive.png");
+
+        // event.preventDefault();
+      });
+    });
